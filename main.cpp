5d2d88a27971c461c1412d190af2722c3eb42c87
Subha-Harini-S <subha.harini@psiog.com>
2025-08-19 14:23:06 +0530

    add

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 #include <fstream>
 using namespace std;
 
+void crud(){
+    cout<<"...";
+    // create();
+    // read();
+    // update();
+    // delete();
+}
+
 int main(){
     cout<<"START\n";
     cout<<"Is sales.csv file exist? y/n\n";
@@ -9,10 +17,21 @@ int main(){
     cin>>ch;
     if(ch=='y'){
         cout<<"Perforn any of the CRUD operation\n";
-        // crud();
+        crud();
     }
     else{
         cout<<"Create sales.csv file\n";
         std::ofstream file("sales.csv");
+    }
+     cout<<"Is temp.csv file exist? y/n\n";
+    char temp;
+    cin>>ch;
+    if(temp=='y'){
+        cout<<"Perforn any of the Sorting operation\n";
+        // sorting();
+    }
+    else{
+        cout<<"Create temp.csv file\n";
+        std::ofstream file("temp.csv");
     }
 }
